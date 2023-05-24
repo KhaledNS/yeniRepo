c7be3835ded05fd10111cb10c54edb01114d4c3b
Enes Kaplan <halit12366@gmail.com>
2023-05-24 18:29:36 +0600

    Version 2

--- src/main/java/Class01.java
+++ src/main/java/Class01.java
@@ -4,6 +4,10 @@ public class Class01 {
         int y = 2;
         System.out.println(x+y);
         System.out.println("Versiyon 1 için işlemler tamam !");
+//      Version 2 Calismalari
+        int z = 3;
+        System.out.println(z+y+x);
+
 
     }
 }
