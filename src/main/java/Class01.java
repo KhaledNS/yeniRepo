48aea76afeea5526e985dc93d5e032dd4a5d6a62
Enes Kaplan <halit12366@gmail.com>
2023-06-02 16:51:27 +0600

    Version 10

--- src/main/java/Class01.java
+++ src/main/java/Class01.java
@@ -4,15 +4,6 @@ public class Class01 {
         int y = 2;
         System.out.println(x+y);
         System.out.println("Versiyon 1 için işlemler tamam !");
-//      Version 2 Calismalari
-        int z = 3;
-        System.out.println(z+y+x);
-//      Version 3 Calismalarina ait alan
-        System.out.println("V3 Kodlari...");
-//      Version 4
-        System.out.println("Version 4 kisminda bu dosyalar alinacak");
-//      Version 5
-        System.out.println("bura da v5 ait");
-
+        System.out.println("yazilar kayit olsun 2");
     }
 }
